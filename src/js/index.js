6fa1f0f10b1cd56f353e697c62743564e7c11fc0
Giovane-Senna <127705280+Giovane-Senna@users.noreply.github.com>
2023-03-28 23:06:11 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaImg(imagem) {
+    let imgIphone = document.querySelector(".foto-iphone")
+    imgIphone.src = imagem
+}
